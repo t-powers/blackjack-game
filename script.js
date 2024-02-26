49add31f9d47ee9e22f59079fe8b97aab5349272
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-26 07:29:32 -0500

    Added starter logic

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstCard = 11
+let secondCart = 10
+let sum = firstCard + secondCart
+
+if ( sum <= 20 ){
+    console.log("Do you want to draw a new card? 🙂")
+}else if ( sum === 21 ){
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}else{
+    console.log("You're out of the game! 😭")
+}
