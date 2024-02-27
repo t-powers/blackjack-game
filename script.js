156c9c6388c428abb0d70ae844b896a397ce4cae
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-26 21:15:56 -0500

    Added second tracking variable

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let firstCard = 11
 let secondCart = 10
 let sum = firstCard + secondCart
 let hasBlackjack = false
+let isAlive = true
 
 if ( sum <= 20 ){
     console.log("Do you want to draw a new card? 🙂")
@@ -10,4 +11,5 @@ if ( sum <= 20 ){
     hasBlackjack = true
 }else{
     console.log("You're out of the game! 😭")
+    isAlive = false
 }
