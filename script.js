5ae0179b1065d360876092e692f04aa2dd29d8fe
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-26 21:15:23 -0500

    Added tracking variable

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let firstCard = 11
 let secondCart = 10
 let sum = firstCard + secondCart
+let hasBlackjack = false
 
 if ( sum <= 20 ){
     console.log("Do you want to draw a new card? 🙂")
 }else if ( sum === 21 ){
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackjack = true
 }else{
     console.log("You're out of the game! 😭")
 }
