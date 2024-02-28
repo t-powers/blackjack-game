ed4ca6359edf2f481ebe69db253833d275199435
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-27 20:15:38 -0500

    Removed c.log to add message variable

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let secondCart = 10
 let sum = firstCard + secondCart
 let hasBlackjack = false
 let isAlive = true
+let message = ""
 
 if ( sum <= 20 ){
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 }else if ( sum === 21 ){
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackjack = true
 }else{
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
     isAlive = false
 }
