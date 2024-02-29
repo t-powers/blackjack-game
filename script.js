9b9d8b05f9e15f8923a6b27a120af4b6c3df056f
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-28 20:40:34 -0500

    Wrapped logic into function

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let hasBlackjack = false
 let isAlive = true
 let message = ""
 
+function startGame(){
 if ( sum <= 20 ){
-    message = "Do you want to draw a new card? 🙂"
+message = "Do you want to draw a new card? 🙂"
 }else if ( sum === 21 ){
     message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackjack = true
 }else{
     message = "You're out of the game! 😭"
     isAlive = false
+}
 }
