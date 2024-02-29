002325c389bf5d976163fee55dffbc36b79c734c
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-28 20:58:58 -0500

    Updated messages

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let message = "";
 
 function startGame() {
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlackjack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
   console.log(message)
