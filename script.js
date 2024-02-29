0a73bec060443962aebfdc4114f729370492738e
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-28 20:45:39 -0500

    Changed button style, modified prettier settings

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let firstCard = 11
-let secondCart = 10
-let sum = firstCard + secondCart
-let hasBlackjack = false
-let isAlive = true
-let message = ""
+let firstCard = 11;
+let secondCart = 10;
+let sum = firstCard + secondCart;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
 
-function startGame(){
-if ( sum <= 20 ){
-message = "Do you want to draw a new card? 🙂"
-}else if ( sum === 21 ){
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackjack = true
-}else{
-    message = "You're out of the game! 😭"
-    isAlive = false
-}
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackjack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
 }
