1642480c162247f83de6a39437c4f33b3bf4f0b8
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-29 07:28:11 -0500

    Added cards-el variable, fixed html variable

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let firstCard = 10;
-let secondCart = 4;
-let sum = firstCard + secondCart;
+let secondCard = 4;
+let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
