62938d2e610cb928b75f0539336b91ffa2005c8f
t-powers <127955989+t-powers@users.noreply.github.com>
2024-03-03 10:25:58 -0500

    Assigned card values to random numbers

--- script.js
+++ script.js
@@ -14,7 +14,14 @@ function startGame() {
 }
 
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function renderGame() {
