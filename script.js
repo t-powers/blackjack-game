b20ebc3405058921af427cad9362663338ff12d6
t-powers <127955989+t-powers@users.noreply.github.com>
2024-03-03 13:16:19 -0500

    New card fx stopped if lost game

--- script.js
+++ script.js
@@ -48,8 +48,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let newCard = getRandomCard();
-  sum += newCard;
-  cards.push(newCard);
-  renderGame();
+  if (isAlive && hasBlackjack === false) {
+    let newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+  }
 }
